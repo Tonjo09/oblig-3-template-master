1b575b4e8d1e966df05af3a14fc3db26365065a7
Tony Nguyen <s341492@oslomet.no>
2022-10-27 23:32:21 +0200

    Fullført oppgave 2 ved hjelp av while-loop som traverserer fra rot, og sjekker antall  noder/ like noder

--- src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
+++ src/main/java/no/oslomet/cs/algdat/Oblig3/SBinTre.java
@@ -169,12 +169,16 @@ public class SBinTre<T> {
     }
 
     public static void main(String[] args) {
-        Integer[] a = {4, 7, 2, 9, 5, 10, 8, 1, 3, 6};
+        Integer[] a = {4,7,2,9,4,10,8,7,4,6};
         SBinTre<Integer> tre = new SBinTre<>(Comparator.naturalOrder());
-        for (int verdi : a) {
-            tre.leggInn(verdi);
-        }
-        System.out.println(tre.antall());  // Utskrift: 10
+        for (int verdi : a) { tre.leggInn(verdi); }
+
+        System.out.println(tre.antall());      // Utskrift: 10
+        System.out.println(tre.antall(5));     // Utskrift: 0
+        System.out.println(tre.antall(4));     // Utskrift: 3
+        System.out.println(tre.antall(7));     // Utskrift: 2
+        System.out.println(tre.antall(10));    // Utskrift: 1
     }
+
 
 } // ObligSBinTre
